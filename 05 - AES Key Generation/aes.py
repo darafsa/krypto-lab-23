4e79f7c52b8d115bdf3c5b97bd9648a4d226d440
darafsa <marek1999@online.de>
2024-03-14 12:05:13 +0100

    change aes functions to return data

--- 05 - AES Key Generation/aes.py
+++ 05 - AES Key Generation/aes.py
@@ -60,33 +60,39 @@ def shift_array(data: list, n: int):
 
 # xors round key to data
 def add_round_key(data: list, key: list):
+	output_data = [None] * len(data)
 	for byte in range(len(data)):
-		data[byte] ^= key[byte]
+		output_data[byte] = data[byte] ^ key[byte]
+	return output_data
 
 
 # substitutes each byte with the corresponding byte of the sbox
 def sub_bytes(data: list, sbox: list):
+	output_data = [None] * len(data)
 	for byte in range(len(data)):
-		data[byte] = sbox[data[byte]]
+		output_data[byte] = sbox[data[byte]]
+	return output_data
 
 
 # moves each line i steps to the left
 def shift_rows(data: list):
-	# new_data = [None] * len(data)
+	output_data = [None] * len(data)
 	for i in range(4):
-		data[i:16:4] = shift_array(data[i:16:4], -i)
-	# for i in range(len(data)):
-	# 	data[i] = new_data[i]
+		output_data[i:16:4] = shift_array(data[i:16:4], -i)
+	return output_data
 
 
 # moves each line i steps to the right
 def shift_rows_inv(data: list):
+	output_data = [None] * len(data)
 	for i in range(4):
-		data[i:16:4] = shift_array(data[i:16:4], i)
+		output_data[i:16:4] = shift_array(data[i:16:4], i)
+	return output_data
 
 
 # mixes the columns of the data
 def mix_columns(data: list):
+	output_data = [None] * len(data)
 	m = [
 		2, 3, 1, 1,
 		1, 2, 3, 1,
@@ -94,11 +100,13 @@ def mix_columns(data: list):
 		3, 1, 1, 2]
 
 	for i in range(4):
-		data[i * 4:(i + 1) * 4] = matrix_mult_vec(m, data[i * 4:(i + 1) * 4])
+		output_data[i * 4:(i + 1) * 4] = matrix_mult_vec(m, data[i * 4:(i + 1) * 4])
+	return output_data
 
 
 # mixes the columns of the data inversely
 def mix_columns_inv(data: list):
+	output_data = [None] * len(data)
 	m = [
 		14, 11, 13, 9,
 		9, 14, 11, 13,
@@ -106,7 +114,8 @@ def mix_columns_inv(data: list):
 		11, 13, 9, 14]
 
 	for i in range(4):
-		data[i * 4:(i + 1) * 4] = matrix_mult_vec(m, data[i * 4:(i + 1) * 4])
+		output_data[i * 4:(i + 1) * 4] = matrix_mult_vec(m, data[i * 4:(i + 1) * 4])
+	return output_data
 
 
 # encrypts 128-bit blocks according to the AES standard
@@ -115,18 +124,20 @@ def encrypt(data: str, key: str, sbox_data: str):
 	round_keys = split_data_in_blocks(key, 128)
 	sbox = split_data_in_blocks(sbox_data, 128 * 16)[0]
 
+	new_blocks = []
 	for block in blocks:
-		add_round_key(block, round_keys[0])
+		new_block = add_round_key(block, round_keys[0])
 		for i in range(1, 10):
-			sub_bytes(block, sbox)
-			shift_rows(block)
-			mix_columns(block)
-			add_round_key(block, round_keys[i])
-		sub_bytes(block, sbox)
-		shift_rows(block)
-		add_round_key(block, round_keys[10])
+			new_block = sub_bytes(new_block, sbox)
+			new_block = shift_rows(new_block)
+			new_block = mix_columns(new_block)
+			new_block = add_round_key(new_block, round_keys[i])
+		new_block = sub_bytes(new_block, sbox)
+		new_block = shift_rows(new_block)
+		new_block = add_round_key(new_block, round_keys[10])
+		new_blocks.append(new_block)
 
-	return "\n".join(" ".join(format(byte, "x").zfill(2) for byte in block) for block in blocks)
+	return "\n".join(" ".join(format(byte, "x").zfill(2) for byte in block) for block in new_blocks)
 
 
 # decrypts 128-bit blocks according to the AES standard
@@ -135,15 +146,17 @@ def decrypt(data: str, key: str, sbox_data: str):
 	round_keys = split_data_in_blocks(key, 128)
 	sbox = split_data_in_blocks(sbox_data, 128 * 16)[0]
 
+	new_blocks = []
 	for block in blocks:
-		add_round_key(block, round_keys[10])
-		shift_rows_inv(block)
-		sub_bytes(block, sbox)
+		new_block = add_round_key(block, round_keys[10])
+		new_block = shift_rows_inv(new_block)
+		new_block = sub_bytes(new_block, sbox)
 		for i in range(9, 0, -1):
-			add_round_key(block, round_keys[i])
-			mix_columns_inv(block)
-			shift_rows_inv(block)
-			sub_bytes(block, sbox)
-		add_round_key(block, round_keys[0])
+			new_block = add_round_key(new_block, round_keys[i])
+			new_block = mix_columns_inv(new_block)
+			new_block = shift_rows_inv(new_block)
+			new_block = sub_bytes(new_block, sbox)
+		new_block = add_round_key(new_block, round_keys[0])
+		new_blocks.append(new_block)
 
-	return "\n".join(" ".join(format(byte, "x").zfill(2) for byte in block) for block in blocks)
+	return "\n".join(" ".join(format(byte, "x").zfill(2) for byte in block) for block in new_blocks)
