3b76beec165a4f65d511de7436ea3555f70be46f
darafsa <marek1999@online.de>
2024-03-15 18:19:39 +0100

    datatype correction

--- 02 - Vigenere Cipher/vigenere_cypher.py
+++ 02 - Vigenere Cipher/vigenere_cypher.py
@@ -5,7 +5,7 @@ def convert_letter_to_number(char: str):
 
 
 # converts a number to an ASCII character, where 0=A and 25=Z
-def convert_number_to_letter(char: str):
+def convert_number_to_letter(char: int):
 	return chr(65 + char)
 
 
